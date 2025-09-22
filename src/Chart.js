a0ab96f55e3990cde97b6c6e4b43f36909792bc2
Jonatan <jw223tb@student.lnu.se>
2025-09-22 18:25:33 +0200

    Add addData with tests

--- src/Chart.js
+++ src/Chart.js
@@ -1,16 +1,27 @@
 export default class Chart {
-    #title;
+    #title
+    #data
 
     constructor(title = "Untitled Chart") {
-        this.#title = title;
+        this.#title = title
+        this.#data = []
 
     }
 
     setTitle(title) {
-        this.#title = title;
+        this.#title = title
     }
 
     getTitle() {
-        return this.#title;
+        return this.#title
+    }
+
+    addData(newData) {
+        for (const [label, value] of Object.entries(newData)) {
+            this.#data.push({ label, value })
+        }
+    }
+    getData() {
+        return this.#data
     }
 }
