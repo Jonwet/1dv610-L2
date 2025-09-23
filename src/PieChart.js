07c05bdddf26f720ceda6b64ba85145856b8ace9
Jonatan <jw223tb@student.lnu.se>
2025-09-23 13:36:50 +0200

    Add VotePercentage in PieChart.js

--- src/PieChart.js
+++ src/PieChart.js
@@ -8,6 +8,14 @@ export default class PieChart extends Chart {
     getTotalVotes() {
         return this.getData().reduce((total, entry) => total + entry.value, 0)
     }
+
+    getVotePercentage() {
+        const totalVotes = this.getTotalVotes()
+        return this.getData().map(entry => ({
+            label: entry.label,
+            percentage: ((entry.value / totalVotes) * 100) + "%"
+        }))
+    }
 }
 
 // Example code:
@@ -15,6 +23,7 @@ const chart = new PieChart("My Pie Chart")
 chart.addData({ Apples: 10, Bananas: 7, Oranges: 15 })
 
 console.log(chart.getTotalVotes()) // Output: 32
+console.log(chart.getVotePercentage()) // Output: [{ label: 'Apples', percentage: 31.25 }, { label: 'Bananas', percentage: 21.875 }, { label: 'Oranges', percentage: 46.875 }]
 
 
 
