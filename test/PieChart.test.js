a2ce2639aea682747e2a82e0c8528e6e638a9c07
Jonatan <jw223tb@student.lnu.se>
2025-09-29 15:01:49 +0200

    Add tests for PieChart.js

---
+++ test/PieChart.test.js
@@ -0,0 +1,38 @@
+import PieChart from "../src/PieChart"
+
+class TestPieChart extends PieChart {}
+
+test("PieChart title can be set and retrieved", () => {
+  const pieChart = new TestPieChart("Test Pie Chart")
+  expect(pieChart.getTitle()).toBe("Test Pie Chart")
+  console.log(pieChart.getTitle())
+})
+
+test("PieChart data can be added and retrieved", () => {
+  const pieChart = new TestPieChart("Test Pie Chart")
+  pieChart.addData({ Apples: 10, Bananas: 7, Cherries: 15 })
+  expect(pieChart.getData()).toEqual([
+    { label: 'Apples', value: 10 },
+    { label: 'Bananas', value: 7 },
+    { label: 'Cherries', value: 15 }
+  ])
+  console.log(pieChart.getData())
+})
+
+test("PieChart can calculate total votes", () => {
+  const pieChart = new TestPieChart("Test Pie Chart")
+  pieChart.addData({ Apples: 10, Bananas: 7, Cherries: 15 })
+  expect(pieChart.getTotalVotes()).toBe(32)
+  console.log(pieChart.getTotalVotes())
+})
+
+test("PieChart can calculate vote percentages", () => {
+  const pieChart = new TestPieChart("Test Pie Chart")
+  pieChart.addData({ Apples: 10, Bananas: 7, Cherries: 15 })
+    expect(pieChart.getVotePercentage()).toEqual([
+    { label: 'Apples', percentage: '31.25%' },
+    { label: 'Bananas', percentage: '21.875%' },
+    { label: 'Cherries', percentage: '46.875%' }
+  ])
+    console.log(pieChart.getVotePercentage())
+})
