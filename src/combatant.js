93eef8265c3b52c8ec095d4aa4c9e49f97a7a567
Jonatan <jw223tb@student.lnu.se>
2025-11-03 17:17:49 +0100

    Rewrote constructor and in turn rewrote my methods

--- src/combatant.js
+++ src/combatant.js
@@ -1,31 +1,65 @@
 class Combatant {
-    constructor(health, attackPower, defense, speed) {
-        this.health = health
-        this.attackPower = attackPower
-        this.defense = defense
-        this.speed = speed
+    constructor(unit) {
+        this.id = unit.id
+        this.name = unit.name
+        this.team = unit.team
+
+        // Unit stats
+        this.maxHealth = unit.maxHealth
+        this.currentHealth = unit.currentHealth
+        this.attackPower = unit.attackPower
+        this.defense = unit.defense
+        this.speed = unit.speed
+
+        // Unit status
+        this.isAlive = true
     }
 
     takeDamage(amount) {
-        this.health = this.health - amount
+        this.currentHealth = this.currentHealth - amount
+
+        if (this.currentHealth <= 0) {
+            this.die()
+        }
+
+        return amount
     }
 
-    healUnit(amount) {
-        this.health = this.health + amount
+    heal(amount) {
+        const oldHealth = this.currentHealth
+        this.currentHealth = Math.min(this.maxHealth, this.currentHealth + amount)
+        return this.currentHealth - oldHealth
     }
 
     die() {
-        this.health = 0
+        this.isAlive = false
     }
 }
 
-const fighter = new Combatant(100, 20, 5, 10)
-console.log(fighter.health)  // 100
+// Tests
+const fighter = new Combatant({
+    id: 1,
+    name: 'Warrior',
+    team: 'A',
+    maxHealth: 100,
+    currentHealth: 100,
+    attackPower: 20,
+    defense: 5,
+    speed: 10
+})
+console.log(fighter.isAlive)  // true
+console.log(fighter.id)  // 1
+console.log(fighter.name)  // 'Warrior'
+console.log(fighter.team)  // 'A'
+console.log(fighter.attackPower)  // 20
+console.log(fighter.defense)  // 5
+console.log(fighter.speed)  // 10
+console.log(fighter.currentHealth)  // 100
 fighter.takeDamage(30)
-console.log(fighter.health)  // 70
+console.log(fighter.currentHealth)  // 70
 
 fighter.healUnit(20)
-console.log(fighter.health)  // 90
+console.log(fighter.currentHealth)  // 90
 
 fighter.die()
-console.log(fighter.health)  // 0
+console.log(fighter.isAlive)  // false
