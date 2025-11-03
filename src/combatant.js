cb31cea1d093519c917697436555ff4791d1d97a
Jonatan <jw223tb@student.lnu.se>
2025-11-03 16:05:43 +0100

    Starting over with a new module, unhappy with my last one

---
+++ src/combatant.js
@@ -0,0 +1,8 @@
+class Combatant {
+    constructor(health, attackPower, defense, speed) {
+        this.health = health
+        this.attackPower = attackPower
+        this.defense = defense
+        this.speed = speed
+    }
+}
