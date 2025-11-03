92afc49d0ab19807ca39f53904170cba9d80b564
Jonatan <jw223tb@student.lnu.se>
2025-11-03 16:32:14 +0100

    Add healUnit and die method

--- src/combatant.js
+++ src/combatant.js
@@ -9,9 +9,23 @@ class Combatant {
     takeDamage(amount) {
         this.health = this.health - amount
     }
+
+    healUnit(amount) {
+        this.health = this.health + amount
+    }
+
+    die() {
+        this.health = 0
+    }
 }
 
 const fighter = new Combatant(100, 20, 5, 10)
 console.log(fighter.health)  // 100
 fighter.takeDamage(30)
 console.log(fighter.health)  // 70
+
+fighter.healUnit(20)
+console.log(fighter.health)  // 90
+
+fighter.die()
+console.log(fighter.health)  // 0
