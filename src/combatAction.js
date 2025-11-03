4960ff8824f4c85805b547580e7a3ae8450cb288
Jonatan <jw223tb@student.lnu.se>
2025-11-03 17:35:36 +0100

    Add combatAction.js

---
+++ src/combatAction.js
@@ -0,0 +1,8 @@
+class combatAction {
+    constructor(action) {
+        this.id = action.id
+        this.name = action.name
+        this.type = action.type
+        this.accuracy = action.accuracy
+    }
+}
